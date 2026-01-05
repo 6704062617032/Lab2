f49ccb182a4ee5d79e53f333931ad2b275beac8f
6704062617032 แผ่นดิน แก้วบรรจง <s6704062617032@email.kmutnb.ac.th>
2026-01-05 11:48:48 +0700

    WS4.3

---
+++ ws43.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int a=0,b=3,c=3,d=3;
+	printf("A =   %f \n",(3*a / 5.0 + 1.0 / b)) ;
+	printf("B =   %f \n",(3 * a + 5 * b) / (2 + c)) ;
+	printf("C =   %f \n",(2.0 / 7.0) * (pow(4, 3 + c) - 5 * d)) ;
+	printf("D =   %f \n",sqrt((2 + 8 * b) / a)) ;
+	printf("E =   %f \n",cbrt(b * b - 4 * d)) ;
+}
